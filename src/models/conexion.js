7711c60f82e0032c103ec3025402442f194fe643
osvaldo <osvaldoplaza43gmail.com>
2024-11-02 23:34:58 -0300

    commit reload

--- src/models/conexion.js
+++ src/models/conexion.js
@@ -4,7 +4,7 @@ const conexion = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: '',
+    password: 'Lnear*07',
     database: 'sis_contable',
     //trustServerCertificate: true
 });
